d5630a2990bdba2b19a63cc16872d88a36be6773
Aaditya-373 <aadityakannan123@gmail.com>
2024-01-25 16:18:03 +0530

    face body and eye detected,now in seperate endpoint for authorized people we feed multiple frames into opencv facial recgonition classifier

--- facetracktest.py
+++ facetracktest.py
@@ -3,8 +3,10 @@ import numpy as np
 
 video_feed = cv.VideoCapture(0)
 
-face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-body_cascade = cv.CascadeClassifier('haarcascade_fullbody.xml')
+face_cascade = cv.CascadeClassifier(
+    './cascades/haarcascade_frontalface_default.xml')
+body_cascade = cv.CascadeClassifier('./cascades/haarcascade_fullbody.xml')
+eye_cascade = cv.CascadeClassifier('./cascades/haarcascade_eye.xml')
 
 while True:
     isTrue, frame = video_feed.read()
@@ -12,24 +14,26 @@ while True:
     grayscale_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(
-        grayscale_frame, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+        grayscale_frame, scaleFactor=1.3, minNeighbors=3, minSize=(30, 30))
     bodies = body_cascade.detectMultiScale(
-        grayscale_frame, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+        grayscale_frame, scaleFactor=1.3, minNeighbors=3, minSize=(30, 30))
+    eyes = eye_cascade.detectMultiScale(
+        grayscale_frame, scaleFactor=1.3, minNeighbors=3, minSize=(30, 30))
 
     for x, y, w, h in faces:
         cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         cv.putText(frame, 'Face', (x, y-10),
                    cv.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
 
-    for x, y, w, h in bodies:
+    for x, y, w, h in eyes:
         cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv.putText(frame, 'Body', (x, y-10),
+        cv.putText(frame, 'Eyes', (x, y-10),
                    cv.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
     cv.imshow('Live surveillance', frame)
 
-    if cv.waitKey(0) & 0xFF == ord('q'):
+    if cv.waitKey(60) & 0xFF == ord('q'):
         break
 
-video_feed.releas()
+video_feed.release()
 cv.destroyAllWindows()
